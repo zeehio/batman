00c993f9233a58a7f295808f51be7f8221834ac3
jie <jie@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-09-25 13:42:33 +0000

    New import

---
+++ pkg/batman/R/plotShift.R
@@ -0,0 +1,245 @@
+plotShift<-function(BM, metaName, plotHist = FALSE, breaks, perMult = FALSE, saveFig = TRUE, saveFigDir = BM$outputDir, prefixFig)
+{ 
+  ## plot multiplet shift posteriors in boxplot or histogram
+  if (missing(BM))
+    return(cat("Please input batman data list.\n"))
+  warnDef<-options("warn")$warn
+  warnRead<-options(warn = -1)
+  ptype = "pdf"
+  mName<-NULL  ## metabolites names
+  mValue<-NULL   ## corresponding ppm values
+  mLabel<-NULL    ## label to be shown in plot
+  mind<-NULL  ## matching multiplets index
+  m<-NULL
+  m<-names(BM$deltaSam)
+  if (!is.null(m)) 
+  { 
+    mN<- strsplit(m, "_")
+  } else {
+    return(cat("No metabolite to match.\n")) 
+  }
+  
+  ns<-5     
+  for (i in 1:length(mN))
+  {
+    mName<-rbind(mName, gsub("[:.:]","-",mN[[i]][1]))
+    mValue<-rbind(mValue,mN[[i]][2])
+  }  
+  
+  mLabel<-paste(mName,mValue,sep=" ")
+  mind <-NULL
+  
+  if (!missing(metaName)) {  
+    meta<-gsub(" ","-",metaName)
+    mind<-which(!is.na(match(tolower(mName),tolower(meta))))    
+  } else {
+    metaName<-NULL
+  }   
+  
+  fc2<-length(mind)
+  if (!is.null(BM$deltaSam)) 
+  {
+    f<-nrow(BM$deltaSam)
+    fc<-ncol(BM$deltaSam)
+    
+    sno<-BM$specRange
+    ind<-fc/length(sno)   ## total number of multiplets
+    ind2<-fc2/length(sno)  ## total number of matching multiplets
+    outpdf1<-NULL
+    perSpec <- NULL       
+    plotAllOP<-NULL
+    if (!perMult || length(sno) == 1)
+    {
+      for (i in 1:length(sno)) 
+      {
+        isno <- sno[i]
+        if (!missing(prefixFig))
+          outpdf1 <- paste(saveFigDir, "/", prefixFig,"_spec_",isno, "_",metaName,"_ppmShift.",ptype, sep="")
+        else
+          outpdf1 <- paste(saveFigDir,"/spec_",isno, "_",metaName,"_ppmShift.",ptype, sep="")
+        #if (!is.null(metaName))
+        x11()
+        if (missing(breaks))
+        {
+          breaks <- length(BM$deltaSam[,1])%/%3
+          if (breaks <=0)
+            breaks <- length(BM$deltaSam[,1])
+        } 
+        ## check if a metabolite name is matched, if not plotting all
+        if (length(mind)>0) 
+        {
+          if (plotHist) 
+          {
+            ## plot result in histogram
+            n<-length(mind[((i-1)*ind2+1):(i*ind2)])
+            if (n <=5)
+              par(mfrow=c(n, 1),oma = c(0, 0, 3, 0))
+            else if (n <= 16 )
+              par(mfrow=c(ceiling(n/4)),4, oma = c(0, 0, 3, 0))
+            else 
+              par(mfrow=c(ceiling(n/5)),5, oma = c(0, 0, 3, 0))
+            for (j in 1:n)
+            {
+              hist(BM$deltaSam[,mind[((i-1)*ind2+1)+j-1]],col="gray",
+                   main = paste(metaName, " at ", mValue[mind[((i-1)*ind2+1)+j-1]], " ppm",sep=""),
+                   xlab = "ppm shift", breaks = breaks, prob=TRUE)
+              lines(density(BM$deltaSam[,mind[((i-1)*ind2+1)+j-1]])) 
+              v<-quantile(BM$deltaSam[,mind[((i-1)*ind2+1)+j-1]],p = c(2.5,97.5)/100)
+              abline(v = v, col = "red", lty=2, lwd = 2)
+              legend("topright", legend = "2.5% & 97.5% quantiles", col = "red", pt.cex=2, lty =  2, cex = 0.8)
+              box()
+            }
+            title(main = list(paste("NMR Spectrum ",isno,": ", BM$specTitle[2,i], sep=""),cex=1.5,font=3), outer=TRUE)                  
+          } else {
+            ## plot result in boxplot
+            boxplot(as.data.frame(BM$deltaSam[,mind[((i-1)*ind2+1):(i*ind2)]]), 
+                    main = paste("NMR Spectrum ", isno, ": ", BM$specTitle[2,i], "\n", metaName, sep=""), 
+                    names = as.character(mValue[mind[((i-1)*ind2+1):(i*ind2)]]),
+                    ylab="ppm shift",xaxt="n") 
+            axis(1,at=1:length(mind[((i-1)*ind2+1):(i*ind2)]),adj=1,padj=0.5,
+                 labels=as.character(mValue[mind[((i-1)*ind2+1):(i*ind2)]]),las=2) 
+          }
+        } else {
+          if (plotHist) 
+          {
+            if (ind <=5)
+              par(mfrow=c(ind, 1),oma = c(0, 0, 3, 0))
+            else if (ind <= 16)
+              par(mfrow=c(4, ceiling(ind/4)),oma = c(0, 0, 3, 0))
+            else 
+              par(mfrow=c(5, ceiling(ind/5)),oma = c(0, 0, 3, 0))
+            for (j in 1:ind)
+            {
+              hist(BM$deltaSam[,((i-1)*ind+1)+j-1],col="gray",
+                   main = paste(mName[((i-1)*ind+1)+j-1], " at ", mValue[((i-1)*ind+1)+j-1], " ppm",sep=""),
+                   xlab = "ppm shift", breaks = breaks, prob=TRUE)
+              lines(density(BM$deltaSam[,((i-1)*ind+1)+j-1]))
+              v<-quantile(BM$deltaSam[,((i-1)*ind+1)+j-1],p = c(2.5,97.5)/100)
+              abline(v = v, col = "red", lty=2, lwd = 2)
+              legend("topright", legend = "2.5% & 97.5% quantiles", col = "red", pt.cex=2, lty =  2, cex = 0.8)
+              box()
+            }
+            title(main = list(paste("NMR Spectrum ",isno,": ", BM$specTitle[2,i], sep=""),cex=1.5,font=3), outer=TRUE)   
+          } else {
+            par(oma = c(4, 0, 0, 0))  
+            boxplot(as.data.frame(BM$deltaSam[,((i-1)*ind+1):(i*ind)]), 
+                    main = paste("NMR Spectrum ", isno, ": ", BM$specTitle[2,i], "\n", metaName, sep=""), 
+                    names = as.character(mLabel[((i-1)*ind+1):(i*ind)]), ylab="ppm shift", xaxt="n")
+            axis(1,at=1:length(mLabel[((i-1)*ind+1):(i*ind)]),adj=1,padj=0.5,
+                 labels=as.character(mLabel[((i-1)*ind+1):(i*ind)]), las=2) 
+          }
+        }
+        if (saveFig) 
+        {
+          if (file.exists(outpdf1))
+            cat("Can't save figure, file", outpdf1, "already exists.\n")
+          else
+            df = dev.copy2pdf(device=x11, file = outpdf1)
+        }
+      }
+    } else {
+      ##  plot per multiplet
+      if (length(mind)>0)
+      {
+        n<-length(mind[((1-1)*ind2+1):(1*ind2)])
+        mLab<-mLabel[mind]
+      } else {
+        #cat("No matching metabolite found for spectrum, ploting all metabolites.\n")
+        n<-ind
+        mLab<-mLabel
+      }
+      for (j in 1:n) 
+      {
+        if (!missing(prefixFig))
+          outpdf1 <- paste(saveFigDir, "/", prefixFig,"_",mLab[j],"_ppmShift.",ptype, sep="")
+        else
+          outpdf1 <- paste(saveFigDir,"/",mLab[j],"_ppmShift.",ptype, sep="") 
+        x11(15,7)
+        
+        if (missing(breaks))
+        {
+          breaks <- length(BM$deltaSam[,1])%/%3
+          if (breaks <=0)
+            breaks <- length(BM$deltaSam[,1])
+        } 
+        ## check if a metabolite name is matched, if not plotting all
+        if (length(mind)>0) 
+        {
+          ## plot result in histogram
+          if (plotHist) 
+          {
+            #n<-length(BM$deltaSam[,mind[((i-1)*ind2+1):(i*ind2)]])
+            if (length(sno) <=5)
+              par(mfrow=c(length(sno), 1),oma = c(0, 0, 3, 0))
+            else if (length(sno) <= 16 )
+              par(mfrow=c(ceiling(length(sno)/4)),4, oma = c(0, 0, 3, 0))
+            else 
+              par(mfrow=c(ceiling(length(sno)/5)),5, oma = c(0, 0, 3, 0))
+            for (i in 1:length(sno))
+            {
+              isno <- sno[i]
+              hist(BM$deltaSam[,mind[((i-1)*ind2+1)+j-1]],col="gray",
+                   main = paste("NMR Spectrum ",isno,": ", BM$specTitle[2,i], sep=""),
+                   xlab = "ppm shift", breaks = breaks,prob=TRUE)
+              lines(density(BM$deltaSam[,mind[((i-1)*ind2+1)+j-1]]))             
+              v<-quantile(BM$deltaSam[,mind[((i-1)*ind2+1)+j-1]],p = c(2.5,97.5)/100)
+              abline(v = v, col = "red", lty=2, lwd = 2)      
+              legend("topright", legend = "2.5% & 97.5% quantiles",
+                     col = "red", pt.cex=2, lty =  2, cex = 0.8)
+              box()
+            }
+            title(main = list(paste(metaName, " at ", mValue[mind[((1-1)*ind2+1)+j-1]]," ppm",sep=""),cex=1.5,font=3), outer=TRUE)                  
+          } else {
+            mtmp<-BM$deltaSam[,mind[seq(1,length(mind),ind2)+j-1]]
+            boxplot(as.data.frame(mtmp), 
+                    main = paste(metaName, " at ", mValue[mind[((1-1)*ind2+1)+j-1]], " ppm",sep=""), 
+                    names = as.character(t(BM$specTitle[2,])),
+                    ylab="ppm shift", xaxt="n") 
+            axis(1,at=1:sno,adj=1,padj=0.5,labels=as.character(t(BM$specTitle[2,])),las=2) 
+          }
+        } else {
+          #cat("No matching metabolite found for spectrum", i, "ploting all multiplets.\n")
+          if (plotHist) 
+          {
+            if (length(sno) <=5)
+              par(mfrow=c(length(sno), 1),oma = c(0, 0, 3, 0))
+            else if (length(sno) <= 16 )
+              par(mfrow=c(ceiling(length(sno)/4)),4,oma = c(0, 0, 3, 0))
+            else 
+              par(mfrow=c(ceiling(length(sno)/5)),5,oma = c(0, 0, 3, 0))
+            for (i in 1:length(sno))
+            {
+              isno <- sno[i]
+              hist(BM$deltaSam[,((i-1)*ind+1)+j-1],col="gray",
+                   main = paste("NMR Spectrum ",isno,": ", BM$specTitle[2,i], sep=""),
+                   xlab = "ppm shift", breaks = breaks,prob=TRUE)
+              lines(density(BM$deltaSam[,((i-1)*ind+1)+j-1]))                            
+              v<-quantile(BM$deltaSam[,((i-1)*ind+1)+j-1],p = c(2.5,97.5)/100)
+              abline(v = v, col = "red", lty=2, lwd = 2)
+              legend("topright", legend = "2.5% & 97.5% quantiles",
+                     col = "red", pt.cex=2, lty =  2, cex = 0.8)
+              box()
+            }
+            title(main = list(paste(mLab[j], " ppm",sep=""),cex=1.5,font=3), outer=TRUE)  
+          } else {
+            ## per Multiplet and ploting boxplot
+            par(oma = c(4, 0, 0, 0))	                 
+            boxplot(as.data.frame(BM$deltaSam[,(seq(1,fc,ind)+j-1)]), 
+                    main = paste(mName[j], " at ", mValue[j], " ppm",sep=""), 
+                    names = as.character(t(BM$specTitle[2,])), ylab="ppm shift", las=2,xaxt="n") 
+            axis(1,at=1:length(sno),adj=1,padj=0.5,labels=as.character(t(BM$specTitle[2,])),las=2)                       
+          }
+        }
+        #title(main = list(paste("ppm shift\nNMR Spectrum ",i,": ", BM$specTitle[2,i], sep=""),cex=1.5,font=3), outer=TRUE)                  
+        if (saveFig) 
+        {
+          if (file.exists(outpdf1))
+            cat("Can't save figure, file", outpdf1, "already exists.\n")
+          else
+            df = dev.copy2pdf(device=x11, file = outpdf1)
+        }
+      }
+    }
+  }
+  warnRead<-options(warn = warnDef)
+}
---
+++ pkg/batman/src/Vpartassign.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+
+using namespace std;
+
+void Vpartassign (vector<double> *b, int start, vector<double> *V) 
+{
+    for (unsigned int i = 0; i < (*V).size(); i++)
+    {
+        (*b)[start+i] = (*V)[i];
+    }    
+}
+
