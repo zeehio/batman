42d0f1e4aedadf44375bffa981b33bab2419b339
jie <jie@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-09-27 14:30:54 +0000

    Mac delete
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/batman@63 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/batman/src/sample_truncated_normal_above.cpp
+++
@@ -1,6 +0,0 @@
-#include "myheader.h"
-
-double sample_truncated_normal_above(double mu, double sigma, double top_lim, rngType * rng)
-{
-    return -sample_truncated_normal_below(-mu, sigma, -top_lim, rng);
-}
--- pkg/batman/src/set_temp.cpp
+++
@@ -1,8 +0,0 @@
-#include "chain_template.h"
-
-void chain_template::set_temp(double Temp1 )
-{
-    Temp=Temp1;
-    for (int lit = 0; lit < pars.s; lit++)
-      spectra[lit].Temp=Temp1;
-}
