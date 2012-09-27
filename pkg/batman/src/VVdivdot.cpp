4e40d2a9547e213b27c3d9e1e0cd512db460c8bd
jie <jie@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-09-27 14:33:06 +0000

    Mac delete
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/batman@65 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/batman/src/VVdivdot.cpp
+++
@@ -1,9 +0,0 @@
-#include <vector>
-using namespace std;
-
-void VVdivdot (vector<double> * V, vector<double> * V2, vector<double> *P )
-{ 
-  for (unsigned int i = 0; i<(*V).size(); i++)
-    (*P)[i] =(*V)[i]/(*V2)[i];      
-}
-
