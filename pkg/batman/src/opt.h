7abf784ec59e4ec37b7cb01bb35b745e991db323
jie <jie@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-09-28 10:24:01 +0000

    Mac import
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/batman@69 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/batman/src/opt.h
@@ -0,0 +1,80 @@
+#ifndef OPT_H
+#define OPT_H
+
+#include <vector>
+using namespace std;
+
+class opt
+{
+ public:
+    string metalist;
+
+    vector<double> st;
+    vector<double> ed;
+    vector<int> nospec;
+    double lowerlimit;
+    int downsample;
+    int ito;
+    int stop_burnin;
+    int savefreq;
+    double spec_freq;
+    double div;
+    double a;
+    double b;
+    //double c;
+    //double d;
+    double log_fwhh_mean;
+    //double centre_shape;
+    double rshape;
+    double log_fwhh_prior_mean;
+    double log_fwhh_prior_var;
+    double log_fwhh_prop_var;
+    double log_fwhh_re_prior_var;
+    double log_fwhh_re_prop_var;
+    double thresh;
+    double steep;
+    double rdelta;
+    int fix;
+    int rerun;
+    int saveHR;
+    int usechemshift;
+    double tempe;
+    long seed;
+
+public:
+    opt()
+    {
+        lowerlimit = -0.5;
+        div = 200000;
+        nospec.assign(1,1);
+        downsample = 10;
+        ito = 5000;
+        stop_burnin = 7000;
+        savefreq = 50;
+        spec_freq = 600;
+        a=0.00001;
+        b=0.000000001;
+        //c=0.0001;
+        //d=100;
+        fix = 2;
+        rerun = 5000;
+        tempe = 1000;
+        saveHR = 1;
+	usechemshift = 0;
+
+	    // these correspond to the fwhh
+	    log_fwhh_prior_mean=0; 
+	    log_fwhh_prior_var=0.1;
+	    log_fwhh_prop_var=0.002;
+	    log_fwhh_re_prior_var=0.0025;
+	    log_fwhh_re_prop_var=0.0001;
+	 
+            
+	    thresh=-0.03;
+	    steep=300;
+	    rdelta = 0.030;
+	    seed = 123983400;
+     }
+} ;
+
+#endif
