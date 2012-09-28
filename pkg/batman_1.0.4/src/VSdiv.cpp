72c41abb49217c80c611f0f9c607dc0d495612d6
jie <jie@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-09-28 12:26:48 +0000

    Mac delete
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/batman@76 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/batman_1.0.4/src/VSdiv.cpp
+++
@@ -1,9 +0,0 @@
-#include <vector>
-using namespace std;
-
-void VSdiv (vector<double> * V, double V2, vector<double> *P )
-{ 
-    for (unsigned int i = 0; i<(*V).size(); i++)
-       (*P)[i] =(*V)[i]/V2;      
-}
-
