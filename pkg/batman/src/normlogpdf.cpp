aef597743d6d506799ce21177df53918557718ed
jie <jie@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-09-28 15:17:31 +0000

    Mac delete

--- pkg/batman/src/normlogpdf.cpp
+++
@@ -1,8 +0,0 @@
-#include <cmath>
-double normlogpdf(double x, double mu, double sigma)
-{
-    double x_std,logpdf;
-    x_std=(x-mu)/sigma;
-    logpdf=-0.918938533-log(sigma)-0.5*pow(x_std,2.0); 
-    return logpdf;
-}
