1af98f507345824d16756d3e62a4a5e2ee752e61
jie <jie@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-10-01 15:30:39 +0000

    Mac delete

--- pkg/batman_1.0.4/src/VVsum.cpp
+++
@@ -1,9 +0,0 @@
-#include <vector>
-using namespace std;
-void VVsum (vector<double> * M, vector<double> * V, vector<double> * P)
-{   
-    for(unsigned int j = 0; j<(*M).size(); j++)
-    {
-        (*P)[j] = (*M)[j]+(*V)[j];
-    }
-}
--- pkg/batman_1.0.4/src/my_betapdf.cpp
+++
@@ -1,9 +0,0 @@
-#include "myheader.h"
-
-double my_betapdf(double x, double a, double b)
-{
-  double p;
-  boost::math::beta_distribution<> myDist(a, b);
-  p =pdf(myDist, x);
-  return p;
-}
