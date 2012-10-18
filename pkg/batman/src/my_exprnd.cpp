a83743ffbe25b1f6087632cbbc13304ba2f53ff2
jie <jie@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-10-18 14:36:36 +0000

    Mac delete

--- pkg/batman/src/my_exprnd.cpp
+++
@@ -1,8 +0,0 @@
-#include "myheader.h"
-
-double my_exprnd( double mu,  rngType *rng) 
-{   
-    boost::exponential_distribution<> gd( 1/mu );  
-    boost::variate_generator<boost::lagged_fibonacci607&,boost::exponential_distribution<> > var_exprnd( (*rng), gd );    
-    return var_exprnd(); 
-}
