7ae927d6f4706a7e13259aee454f71a2a9d7b289
jie <jie@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-02-12 16:26:18 +0000

    Mac delete

--- pkg/batman/src/deltaadapt.cpp
+++
@@ -1,8 +0,0 @@
-#include "myheader.h"
-
-double deltaadapt(double noadapt)
-{
-	// written by Dr. Jie Hao, Dr William Astle
-    return min(0.5,3/sqrt(noadapt));
-}
-
--- pkg/batman/src/set_temp.cpp
+++
@@ -1,9 +0,0 @@
-// written by Dr. Jie Hao, Dr William Astle
-#include "chain_template.h"
-
-void chain_template::set_temp(double Temp1 )
-{
-    Temp=Temp1;
-    for (int lit = 0; lit < pars.s; lit++)
-      spectra[lit].Temp=Temp1;
-}
