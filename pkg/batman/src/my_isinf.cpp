6d9c24f64101dc0dc71c479c93f45de52cd74ed4
jie <jie@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-02-12 16:26:18 +0000

    Mac delete
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/batman@119 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/batman/src/my_isinf.cpp
+++
@@ -1,7 +0,0 @@
-// written by Dr. Jie Hao, Dr William Astle
-#include <cmath>
-using namespace std;
-bool my_isinf(double x)
-{
-    return isinf(x);
-}
--- pkg/batman/src/my_normcdf.cpp
+++
@@ -1,9 +0,0 @@
-// written by Dr. Jie Hao, Dr William Astle
-#include "myheader.h"
-double my_normcdf(double x, double mu, double sigma)
-{
-    double p;
-    boost::math::normal myDist(mu, sigma);
-    p = cdf(myDist, x);
-    return p;
-}
