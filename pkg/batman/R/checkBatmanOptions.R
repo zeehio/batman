c055f3716c615fbac1258a5ff73afc30dc4a2057
jie <jie@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-07-25 16:37:28 +0000

    batman options rename parameters

--- pkg/batman/R/checkBatmanOptions.R
+++ pkg/batman/R/checkBatmanOptions.R
@@ -1,10 +1,12 @@
-checkBatmanOptions<-function(dir = dir1)
+checkBatmanOptions<-function(dir)
 {
-## written by Dr. Jie Hao, Imperial College London
+  ## written by Dr. Jie Hao, Imperial College London
   content <-read.table(dir,sep="\n",comment.char = "")
   
   iscomment <- substr(content[,1],1,1)
+  # comment lines
   cid <- which(iscomment == '%')
+  # parameter lines
   cidn <- which(iscomment != '%')
   co <- as.matrix(content)
   
@@ -13,32 +15,62 @@ checkBatmanOptions<-function(dir = dir1)
   
   cend <- which(cid > cidn[rid])
   
-  if (firsts[2] != "Spectr" )
+  if (firsts[2] != 'Spectr' )
   {
-    myVector <- strsplit(co[cidn[2],1], ":")
-    co[cidn[2],1]<- c(paste('Spectra range to be included: ', myVector[[1]][2], sep = ""))
+    if (firsts[2] != 'specNo')
+    {
+      myVector <- strsplit(co[cidn[2],1], ":")
+      co[cidn[2],1]<- c(paste('specNo - Ranges of spectra number to be included (e.g. 1,3-4 etc.): ', myVector[[1]][2], sep = "")) 
+    }
   }
   if (length(cidn) > rid)
-  {
-    
+  {    
     if (firsts[rid+1] != 'Use sp')
     {
-      if (length(cend) == 0)
+      if (firsts[rid+1] != 'csFlag')
       {
-        co[rid+1,1]<- c('Use specified chemical shift for spectra (chemShiftperSpectra.csv) file (1/0): 0')
-      }
-      else if (cid[cend[length(cend)]]<dim(content)[1])
-      {
-        co[cid[cend[length(cend)]]+1,1]<- c('Use specified chemical shift for spectra (chemShiftperSpectra.csv) file (1/0): 0')
-      }
-      else
-      {
-        co<- rbind(co, 'Use specified chemical shift for spectra (chemShiftperSpectra.csv) file (1/0): 0')
-      }
-    } 
-    
+        if (length(cend) == 0)
+        {
+          co[rid+1,1]<- c('csFlag - Specify chemical shift for each multiplet in each spectrum? (chemShiftperSpectra.csv file) (Yes - 1 / No - 0): 0')
+        }
+        else if (cid[cend[length(cend)]]<dim(content)[1])
+        {
+          co[cid[cend[length(cend)]]+1,1]<- c('csFlag - Specify chemical shift for each multiplet in each spectrum? (chemShiftperSpectra.csv file) (Yes - 1 / No - 0): 0')
+        }
+        else
+        {
+          co<- rbind(co, 'csFlag - Specify chemical shift for each multiplet in each spectrum? (chemShiftperSpectra.csv file) (Yes - 1 / No - 0): 0')
+        }
+      } 
+    }
   } else {
-    co<- rbind(co, 'Use specified chemical shift for spectra (chemShiftperSpectra.csv) file (1/0): 0')
+    co<- rbind(co, 'csFlag - Specify chemical shift for each multiplet in each spectrum? (chemShiftperSpectra.csv file) (Yes - 1 / No - 0): 0')
+  }
+  
+  if (firsts[1] != 'ppmRan')
+  {
+    dirsys <- paste(system.file("extdata",package="batman"), '/batmanOptions.txt', sep = '')
+    contentsys <-read.table(dirsys,sep="\n",comment.char = "")
+    
+    iscommentsys <- substr(contentsys[,1],1,1)
+    # comment lines
+    cidsys <- which(iscommentsys == '%')
+    # parameter lines
+    cidnsys <- which(iscommentsys != '%')
+    cosys <- as.matrix(contentsys)
+    
+    for (i in 1:length(cidnsys))
+    {
+      myV1 <- strsplit(cosys[cidnsys[i],1], ":")
+      myV2 <- strsplit(co[cidn[i],1], ":")
+      cosys[cidnsys[i],1] <- c(paste(myV1[[1]][1], ':', myV2[[1]][2], sep = ""))
+    }  
+    if (cid[length(cid)] > cidn[length(cidn)])
+    {
+      for (j in (cidn[length(cidnsys)]+1):length(co))
+      cosys<- rbind(cosys, co[j])      
+    }
+    co <- cosys
   }
   write.table(co, file = dir, sep = "\n", row.names = FALSE,col.names = FALSE,quote=FALSE)
 }
