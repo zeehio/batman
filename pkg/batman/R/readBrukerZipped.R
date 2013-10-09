f7dcae03617cbf452255051947bf17c921fae6bd
jie <jie@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-10-09 13:08:52 +0000

    batman readBrukerZipped

--- pkg/batman/R/readBrukerZipped.R
+++ pkg/batman/R/readBrukerZipped.R
@@ -9,15 +9,23 @@ readBrukerZipped<-function(BrukerDataZipDir)
   zipfile <-list.files(path = datapath, pattern = "*.zip$", all.files = FALSE,full.names = TRUE, recursive = TRUE, ignore.case = TRUE)
   if (length(zipfile)>0)
   {
-  for (i in 1:length(zipfile))
+    for (i in 1:length(zipfile))
+    {
+      unzip(zipfile[i], exdir = datapath)
+      unzipfile[i] <- substr(zipfile[i], 1, nchar(zipfile[1])-4)
+    }
+  } else if (substr(datapath,nchar(datapath)-3,nchar(datapath)) == '.zip')
   {
-    unzip(zipfile[i], exdir = datapath)
-    unzipfile[i] <- substr(zipfile[i], 1, nchar(zipfile[1])-4)
+    zipfile <- datapath
+    ns <- strsplit(zipfile, "/")
+    ns <- ns[[1]]
+    datapath <- substr(zipfile, 1, nchar(zipfile)-nchar(ns[length(ns)])-1)
+    unzip(zipfile, exdir = datapath)
+    unzipfile <- substr(zipfile, 1, nchar(zipfile)-4)
   }
-  }
-
+  
   sa<-readBruker(datapath)
-  if (length(zipfile)>0)
+  if (length(unzipfile)>0)
   {
     unlink(unzipfile, recursive = TRUE, force = FALSE)
   }
