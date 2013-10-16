df7e208ad4db0550ad39565697244b19bac51dd8
jie <jie@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-10-16 17:09:22 +0000

    batman save only post

--- pkg/batman/src/yeast_analysis_Rw.cpp
+++ pkg/batman/src/yeast_analysis_Rw.cpp
@@ -63,7 +63,7 @@
 
         rngType rng(options.seed);
 
-        int iteration = options.ito;
+		int iteration = options.ito;
         iteration = itR;
 
         matrix Data;
@@ -212,7 +212,7 @@
                     seconds7 = time (NULL);
                     Rprintf("\ntime used for burnin is %i seconds.\n", seconds7-seconds);
                 }
-                if (options.savefreq!=0)
+                if (options.savefreq!=0 && ito<options.stop_burnin-1)
                 {
                     if(chain.it%options.savefreq == 0 || chain.it== iteration)
 					{
