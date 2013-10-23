85d4a76270a9697319a3423c7df689d904cb98fd
jie <jie@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-10-23 16:37:53 +0000

    batman emice data
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/batman@238 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/batman/R/newDir.R
+++ pkg/batman/R/newDir.R
@@ -14,7 +14,11 @@ newDir<-function(runBATMANDir = getwd(), overwriteFile = FALSE)
   if(!file.exists(dirPST)){
     dir.create(dirPST)
   }
-  dirTPure<-paste(dirIP,"/testBrukerPureSpec",sep="")
+  
+  dirRastGln<-paste(dirPST,"/L-Glutamine.txt",sep="")
+  cp<-file.copy(dirRastGln,dirPST,overwrite=overwriteFile)
+  
+  dirTPure<-paste(dirIP,"/testBrukerPureSpecForRasterTemplate",sep="")
   if(!file.exists(dirTPure)){
 		dir.create(dirTPure)
 	}
@@ -23,19 +27,14 @@ newDir<-function(runBATMANDir = getwd(), overwriteFile = FALSE)
   if(!file.exists(dirTPure1)){
     dir.create(dirTPure1)
   }
-  
-  dirTPure2<-paste(dirTPure,"/testPure2",sep="")
-  if(!file.exists(dirTPure2)){
-    dir.create(dirTPure2)
-  }
-  
+
   dirOP<-paste(dirRoot,"/BatmanOutput",sep="")
   if(!file.exists(dirOP)) {
     dir.create(dirOP)
   }
   extdir<-system.file("extdata",package="batman")
 	
-	src.dir <- paste(extdir,"/testBrukerPureSpec/testPure1",sep="")
+	src.dir <- paste(extdir,"/testBrukerPureSpecForRasterTemplate/testPure1",sep="")
 	dest.dir <-dirTPure1
 	
 	file.names <- dir(src.dir)
@@ -44,14 +43,6 @@ newDir<-function(runBATMANDir = getwd(), overwriteFile = FALSE)
 					 to=paste(dest.dir, x, sep="/"),
 					 overwrite = FALSE) })
 	
-	src.dir <- paste(extdir,"/testBrukerPureSpec/testPure2",sep="")
-	dest.dir <-dirTPure2
-	
-	file.names <- dir(src.dir)
-	sapply(file.names, function(x) {
-		   file.copy(from=paste(src.dir, x, sep="/"),
-					 to=paste(dest.dir, x, sep="/"),
-					 overwrite = FALSE) })
 	
   dirbo<-paste(extdir,"/batmanOptions.txt",sep="")
   cp<-file.copy(dirbo,dirIP, overwrite=overwriteFile)
@@ -63,6 +54,9 @@ newDir<-function(runBATMANDir = getwd(), overwriteFile = FALSE)
   cp<-file.copy(dirR,dirIP,overwrite=overwriteFile)
   dirRU<-paste(extdir,"/multi_data_user.csv",sep="")
   cp<-file.copy(dirRU,dirIP,overwrite=overwriteFile)
+  
+  dirRU_adj<-paste(extdir,"/multi_data_user_adj.csv",sep="")
+  cp<-file.copy(dirRU_adj,dirIP,overwrite=overwriteFile)
   
   dirA<-c(dirRoot, dirIP, dirOP, dirPST, dirTPure)
   ##, dirPST
